6e88c2de1bfb0ec320b2c64f6ec930da3bc939ae
Ottwebdlbot <163734219+Ottwebdlbot@users.noreply.github.com>
2024-08-04 15:43:34 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -46,11 +46,11 @@ async def start(bot, update):
             [
                 [
                     InlineKeyboardButton(
-                        "Source code ⚡", url="https://github.com/Clinton-Abraham/UPLOADER-BOT"
+                        "Source code ⚡", url="https://github.com/ottwebdlbot/UPLOADER-BOT"
                     ),
-                    InlineKeyboardButton("Project Channel 👨🏻‍💻", url="https://t.me/Space_X_bots"),
+                    InlineKeyboardButton("Project Channel 👨🏻‍💻", url="https://t.me/TCP_Bots"),
                 ],
-                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/clinton_abraham")],
+                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/mr_provider")],
             ]
         ),
         reply_to_message_id=update.message_id
