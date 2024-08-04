2a7a2b1fbdfdbad5fac2ac18d5d71e2806703ebd
Ottwebdlbot <163734219+Ottwebdlbot@users.noreply.github.com>
2024-08-04 15:40:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = "7367548692:AAH5eklnBHQ2Q_-o_5GDcEex6HUd3Gfy_E4"
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = 23746596
+    API_HASH = "15ce5501fd05fae38473f02a97fdf899"
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -24,9 +24,9 @@ class Config(object):
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    SESSION_NAME = "UPLOADER-X-BOT"
+    OWNER_ID = 6116993643
+    SESSION_NAME = "TCP-URLUPLOADER-BOT"
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = "mongodb+srv://shivaynetflix2:Shivay@1234@cluster0.z0uewqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     MAX_RESULTS = "50"
     PREMIUM_USER = os.environ.get("PREMIUM_USER")
